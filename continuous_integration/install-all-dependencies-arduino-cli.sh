dcb50cf26e6a5190d36ce1ed3060aa9e463bf59f
Sara Damiano <sdamiano@stroudcenter.org>
2025-07-04 12:47:08 -0400

    Added github action to check compilation
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

---
+++ continuous_integration/install-all-dependencies-arduino-cli.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Set options,
+set -e # Exit with nonzero exit code if anything fails
+if [ "$RUNNER_DEBUG" = "1" ]; then
+    echo "Enabling debugging!"
+    set -v # Prints shell input lines as they are read.
+    set -x # Print command traces before executing command.
+fi
+
+echo "\e[32mCurrent Arduino CLI version:\e[0m"
+arduino-cli version
+
+echo "\e[32mUnzipping the library\e[0m"
+unzip -o AllDependencies.zip -d home/arduino/downloads/libs -x "*.git/*" "continuous_integration/*" "docs/*" "examples/*"
+
+echo "\e[32mEnsuring no old directories exist\e[0m"
+rm -r -R home/arduino/user/libraries/*
+
+echo "\e[32mMoving the unzipped libraries to the libraries directory\e[0m"
+mv home/arduino/downloads/libs/* home/arduino/user/libraries/
+
+echo "\e[32mUpdating the library index\e[0m"
+arduino-cli --config-file continuous_integration/arduino_cli.yaml lib update-index
+
+echo "\e[32mListing libraries detected by the Arduino CLI\e[0m"
+arduino-cli --config-file continuous_integration/arduino_cli.yaml lib list
+
+echo "\e[32mListing the contents of the Arduino library directory\e[0m"
+ls home/arduino/user/libraries
