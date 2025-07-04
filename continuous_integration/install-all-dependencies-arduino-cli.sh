cfbc64399e4db7b9c5c465be9132d23c0fd98bfd
Sara Damiano <sdamiano@stroudcenter.org>
2025-07-04 12:59:54 -0400

    Fix to dep install
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- continuous_integration/install-all-dependencies-arduino-cli.sh
+++ continuous_integration/install-all-dependencies-arduino-cli.sh
@@ -12,10 +12,12 @@ echo "\e[32mCurrent Arduino CLI version:\e[0m"
 arduino-cli version
 
 echo "\e[32mUnzipping the library\e[0m"
-unzip -o AllDependencies.zip -d home/arduino/downloads/libs -x "*.git/*" "continuous_integration/*" "docs/*" "examples/*"
+unzip -o AllDependencies.zip -d home/arduino/downloads/libs -x "*.git/*" "continuous_integration/*" "docs/*" "examples/*" "*.history/*"
 
-echo "\e[32mEnsuring no old directories exist\e[0m"
-rm -r -R home/arduino/user/libraries/*
+if [ -d "home/arduino/user/libraries" ]; then
+    echo "\e[32mEnsuring no old directories exist\e[0m"
+    rm -rf home/arduino/user/libraries/*
+fi
 
 echo "\e[32mMoving the unzipped libraries to the libraries directory\e[0m"
 mv home/arduino/downloads/libs/* home/arduino/user/libraries/
